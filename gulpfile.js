b78c0b4b97399187ccfd136ff5122ca4c40172d0
aazapata <aazapata@smdigital.com.co>
2016-12-04 23:33:54 -0500

    Prueba de unificar js

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,27 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var concat = require('gulp-concat');
 
 //Tarea para compilar sass
-gulp.task('compilarsass', function() {
+gulp.task('sass', function() {
     return gulp.src('scss/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('css/main.css'));
+        .pipe(sass({
+        	ouputStyle: 'compressed'
+        }))
+        .pipe(gulp.dest('css'));
+});
+
+//Tarea para unir los js
+gulp.task('scripts', function() {
+  return gulp.src('js/*.js')
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest('js/'));
 });
 
 // Watch Files For Changes
-gulp.task('watch', function() {
-    gulp.watch('scss/*.scss', ['compilarsass']);
+gulp.task('compilarsass', function() {
+    gulp.watch('scss/*.scss', ['sass']);
 });
 
 // Default Task
-gulp.task('default', ['watch']);
+gulp.task('default', ['compilarsass']);
