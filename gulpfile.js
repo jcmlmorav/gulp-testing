0d65fb52a63607dcef7d4ad8ba5a862de3772485
Juan Camilo Mora <jcmora@smdigital.com.co>
2016-12-05 09:40:41 -0500

    Merged joagudelo

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,14 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var cleanCSS = require('gulp-clean-css');
-var concat = require('gulp-concat');
-var jsmin = require('gulp-jsmin');
-var rename = require('gulp-rename');
 
-gulp.task('sass', function () {
-  return gulp.src('./scss/app.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(rename('main.css'))
-    .pipe(gulp.dest('./css/'));
+gulp.task('sass', function() {
+	return gulp.src('scss/style.sass') // Leer un archivo
+		.pipe(sass()) // Compilar SASS
+		.pipe(gulp.dest('css')); // Guardar archivo
 });
 
-gulp.task('minify-css', function() {
-  return gulp.src('css/main.css')
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('css/'));
+gulp.task('watch', function() {
+	gulp.watch('scss/style.sass', ['sass']);
 });
 
-gulp.task('concat-js', function() {
-    gulp.src([
-            'bower_components/jquery-1.11.1/dist/jquery.min.js',
-            'js/*.js'
-        ])
-        .pipe(concat('app.js'))
-        .pipe(gulp.dest('js'));
-});
-
-gulp.task('minify-js', function () {
-    gulp.src('js/app.js')
-        .pipe(jsmin())
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('js'));
-});
-
-gulp.task('default', ['sass', 'minify-css', 'minify-js']);
+gulp.task('default', ['sass']);
